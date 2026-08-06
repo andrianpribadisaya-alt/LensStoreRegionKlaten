83cf287e5416e7a9fcebb31792bd1a36cb6543ef
andrianpribadisaya-alt <andrianpribadisaya@gmail.com>
2026-08-06 14:17:23 +0700

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  compress: true,
+
+  experimental: {
+    typedRoutes: true
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  }
+};
+
+export default nextConfig;
