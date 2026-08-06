3bb320ba204110d6e3a28cc14fa82ce6aa722a09
andrianpribadisaya-alt <andrianpribadisaya@gmail.com>
2026-08-06 17:11:36 +0700

    Create layout.tsx

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import AdminLayout from "@/components/admin/AdminLayout";
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+    return (
+
+        <AdminLayout>
+
+            {children}
+
+        </AdminLayout>
+
+    );
+
+}
