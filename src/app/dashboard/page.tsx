9cc2a9a54a3a50d8facfaa7e7c2d5a57458ee893
andrianpribadisaya-alt <andrianpribadisaya@gmail.com>
2026-08-06 16:42:20 +0700

    Create page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold text-white">
+        Selamat Datang di LensOtp 👋
+      </h2>
+
+      <p className="mt-2 text-slate-400">
+        Dashboard masih dalam tahap pengembangan.
+      </p>
+    </div>
+  );
+}
