c6cc7ec31cc7049dd8c305b3da4ea45e72dff198
andrianpribadisaya-alt <andrianpribadisaya@gmail.com>
2026-08-06 17:15:20 +0700

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,37 @@
+import DashboardCard from "@/components/admin/DashboardCard";
+
 export default function AdminPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">
-        Admin Dashboard
-      </h1>
-    </div>
-  );
+
+    return (
+
+        <div className="space-y-6">
+
+            <div className="grid md:grid-cols-4 gap-5">
+
+                <DashboardCard
+                    title="Total User"
+                    value="0"
+                />
+
+                <DashboardCard
+                    title="Total Deposit"
+                    value="Rp0"
+                />
+
+                <DashboardCard
+                    title="Total Order"
+                    value="0"
+                />
+
+                <DashboardCard
+                    title="Revenue"
+                    value="Rp0"
+                />
+
+            </div>
+
+        </div>
+
+    );
+
 }
