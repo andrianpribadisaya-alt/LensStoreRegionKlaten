20b124f1b88868558c770752fb80bcdf13ccd56f
andrianpribadisaya-alt <andrianpribadisaya@gmail.com>
2026-08-07 13:53:25 +0700

    Update test-action.ts

--- src/app/admin/provider/test-action.ts
+++ src/app/admin/provider/test-action.ts
@@ -1,1 +1,10 @@
+"use server";
 
+import { connectDB } from "@/lib/db";
+import { testSMSCode } from "@/services/smscode";
+
+export async function testConnection() {
+  await connectDB();
+
+  return await testSMSCode();
+}
